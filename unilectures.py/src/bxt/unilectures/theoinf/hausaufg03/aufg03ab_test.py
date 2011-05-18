02169bacb3f438300c99ce5d771bc975a650a911
Bernhard Häussner <bxt@die-optimisten.net>
2011-05-18 14:22:36 +0200

    Added aufg3ab tests

---
+++ unilectures.py/src/bxt/unilectures/theoinf/hausaufg03/aufg03ab_test.py
@@ -0,0 +1,55 @@
+from aufg03ab import h,fN
+
+
+
+print("fN testen...")
+
+print(fN(0,1)==0)
+print(fN(0,2)==0)
+print (fN(0,3)==0)
+
+print (fN(1,1)==1)
+print (fN(1,2)==2)
+print (fN(1,3)==1)
+
+print (fN(2,1)==1)
+print (fN(2,2)==2)
+print (fN(2,3)==1)
+
+print (fN(3,1)==7)
+print (fN(3,2)==8)
+print (fN(3,3)==3)
+
+
+
+print("h testen...")
+
+print(h(-2,-2)==-2)
+print(h(-2,-1)==-5)
+print(h(-2,1)==2)
+print(h(-2,2)==1)
+
+print(h(-1,-2)==0)
+print(h(-1,-1)==-1)
+print(h(-1,1)==0)
+print(h(-1,2)==0)
+
+print(h(0,-2)==0)
+print(h(0,-1)==0)
+print(h(0,1)==0)
+print(h(0,2)==0)
+
+print(h(1,-2)==-1)
+print(h(1,-1)==2)
+print(h(1,1)==1)
+print(h(1,2)==-1)
+
+print(h(2,-2)==-3)
+print(h(2,-1)==8)
+print(h(2,1)==4)
+print(h(2,2)==2)
+
+
+
+
+
