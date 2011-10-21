071ad78a538507d3fd4b89c8fc85f7819697b8ce
Bernhard Häussner <bxt@die-optimisten.net>
2011-10-21 14:55:58 +0200

    Test all sorting algos at once

--- unilectures/src/bxt/unilectures/algorithmenunddatenstrukturen/fun/sorting/SortMain.java
+++ unilectures/src/bxt/unilectures/algorithmenunddatenstrukturen/fun/sorting/SortMain.java
@@ -9,13 +9,15 @@ public class SortMain {
 	 */
 	public static void main(String[] args) {
 		
-		//Sort sort = new InsertionSort();
-		Sort sort = new BubbleSort();
-		long[] a = {4,6,3,5,8,9,7,2,1};
-		System.out.println(Arrays.toString(a));
-		sort.sort(a);
-		System.out.println(Arrays.toString(a));
-		
+		Sort[] sortList={new InsertionSort(),new BubbleSort()};
+		for (Sort sort : sortList) {
+			System.out.println(String.format(
+					"Sortiere mit %s:",sort.getClass().getSimpleName()));
+			long[] a = {4,6,3,5,8,9,7,2,1};
+			System.out.println(Arrays.toString(a));
+			sort.sort(a);
+			System.out.println(Arrays.toString(a));
+		}
 	}
 
 }
