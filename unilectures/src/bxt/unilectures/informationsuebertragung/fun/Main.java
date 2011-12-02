15175c30b40d6d60dc25d1a869a1ba8478ec5511
Bernhard Häussner <bxt@die-optimisten.net>
2011-12-02 02:15:56 +0100

    Added many new calculations to Counts.java

--- unilectures/src/bxt/unilectures/informationsuebertragung/fun/Main.java
+++ unilectures/src/bxt/unilectures/informationsuebertragung/fun/Main.java
@@ -4,6 +4,8 @@ import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
 
+import bxt.unilectures.informationsuebertragung.fun.Counts.Unit;
+
 public class Main {
 
 	/**
@@ -12,7 +14,9 @@ public class Main {
 	 * Will produce informative output while performing the following steps:
 	 * <ul>
 	 * <li> Convert bytes to hytes and utf-8 string
+	 * <li> Count probabilities and dump some facts about them
 	 * <li> Calculate a code table from probabilities
+	 * <li> Show some facts about the code table
 	 * <li> Endoce the hytes to a compressed binary
 	 * <li> Display comression ratio
 	 * <li> Dedoce the compressed binary to hytes again 
@@ -28,7 +32,12 @@ public class Main {
 		byte[] input = {0x42, 0x65, 0x72, 0x6E, 0x68, 0x61, 0x72, 
 				0x64, 0x20, 0x48, (byte)0xC3, (byte)0xA4, 0x75, 
 				0x73, 0x73, 0x6E, 0x65, 0x72};
-				
+		
+		/*
+		// another testing value:
+		byte[] input="aaaaaaltaaaaaaaaaaaaaaaaah!".getBytes(charset);
+		*/
+		
 		String textInput = new String(input,charset);
 		System.out.println("Data interpreted as UTF-8 text: "+textInput);
 		System.out.println("  Size: "+input.length*8+" bits");
@@ -39,6 +48,31 @@ public class Main {
 		System.out.println("  Size: "+hytes.size()*4+" Bits");
 		
 		Counts<Hyte> counts = new Counts<Hyte>(hytes);
+		
+		System.out.println("  Probability of H6");
+		System.out.printf("    %.3f %%\n",
+				counts.getProbability(Hyte.H6));
+		System.out.println("  Self Information of H6");
+		System.out.printf("    %.3f bits\n",
+				counts.getOneSelfInformation(Hyte.H6,Unit.BITS));
+		System.out.println("  Probability of HC");
+		System.out.printf("    %.3f %%\n",
+				counts.getProbability(Hyte.HC));
+		System.out.println("  Self Information of HC");
+		System.out.printf("    %.3f bits\n",
+				counts.getOneSelfInformation(Hyte.HC,Unit.BITS));
+		
+		System.out.println("  Maximum Self Information, equal probabilities");
+		System.out.printf("    %.3f bits\n",
+				counts.getMaximumSelfInformation(Unit.BITS));
+		System.out.println("  Actual Self Information");
+		System.out.printf("    %.3f bits\n",
+				counts.getSelfInformation(Unit.BITS));
+		System.out.println("  Input self information");
+		System.out.printf("    %.3f bits\n",
+				counts.getSelfInformation(Unit.BITS)*hytes.size() );
+		
+		System.out.print("\n\n");
 		
 		CodeStrategy[] codeStrategies = {
 				new Huffman(),
@@ -53,15 +87,28 @@ public class Main {
 			Map<Hyte,List<Boolean>> codeTable = codeStrategy.getCodeTable(counts);
 			System.out.print(codeTableToString(codeTable));
 			
+			System.out.println("  Mean symbol length");
+			System.out.printf("    %.3f bits\n",
+					counts.getMeanLength(codeTable));
+			System.out.println("  Redundancy");
+			System.out.printf("    %.3f bits\n",
+					counts.getRedundancy(codeTable,Unit.BITS));
+			System.out.println("Expected Compression Ratio");
+			System.out.printf("    %.3f %%\n", 
+					counts.getCompressionRatio(codeTable,Unit.BITS));
+			
 			Code<Hyte> code = new Code<Hyte>(codeTable);
 			
 			List<Boolean> compressed = code.encode(hytes);
 			System.out.println("Compressed: "+booleanListToString(compressed));
 			System.out.print("  Size: "+compressed.size()+" Bits");
-			System.out.println(" ("+(float)compressed.size()/(input.length*8)+"%)");
+			System.out.printf(" (%.3f %%)\n",
+					(float)compressed.size()/(input.length*8));
 			
 			restored = code.decode(compressed);
 		
+			System.out.print("\n\n");
+			
 		}
 		
 		
