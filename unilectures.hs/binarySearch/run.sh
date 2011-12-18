f49698ba45a6c1f8620edfbcfcb188efcfbd1d59
Bernhard Häussner <bxt@die-optimisten.net>
2011-12-18 16:12:50 +0100

    Enabled cabal

---
+++ unilectures.hs/binarySearch/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Configuring for local"
+cabal configure --user
+echo "Compiling"
+cabal build
+echo "Generating docs"
+cabal haddock --executables --internal --hyperlink-source
+echo "Running Main.hs"
+./dist/build/ads-binarySearch/ads-binarySearch
+
+# cabal clean
