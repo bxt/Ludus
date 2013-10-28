fcd81976ceef4fa284651e4accd896ceef786c3a
Bernhard Häussner <bxt@die-optimisten.net>
2013-10-28 23:57:43 +0100

    Add constructors to monsters.rs

--- unilectures.rs/monsters.rs
+++ unilectures.rs/monsters.rs
@@ -1,5 +1,6 @@
 trait Monster {
   fn attack(&self);
+  fn new() -> Self;
 }
 
 struct SpiderMonkey {
@@ -18,18 +19,21 @@ impl Monster for SpiderMonkey {
   fn attack(&self) {
     println(fmt!("The spider monkey attacks for %d",self.legs*2))
   }
+  fn new() -> SpiderMonkey { SpiderMonkey {legs: 8} }
 }
 
 impl Monster for RubyGargoyle {
   fn attack(&self) {
     println(fmt!("The ruby gargoyle attacks for %d",self.strength))
   }
+  fn new() -> RubyGargoyle {RubyGargoyle {strength: 15} }
 }
 
 impl Monster for TigerChimera {
   fn attack(&self) {
     println(fmt!("The tiger chimera attacks for %d",self.strength))
   }
+  fn new() -> TigerChimera { TigerChimera {strength: 17} }
 }
 
 fn monsters_attack(monsters: &[~Monster]) {
@@ -39,9 +43,9 @@ fn monsters_attack(monsters: &[~Monster]) {
 }
 
 fn main() {
-  let monkey   = ~SpiderMonkey {legs: 8};
-  let gargoyle = ~RubyGargoyle {strength: 15};
-  let chimera  = ~TigerChimera {strength: 17};
+  let monkey:   ~SpiderMonkey = ~Monster::new();
+  let gargoyle: ~RubyGargoyle = ~Monster::new();
+  let chimera:  ~TigerChimera = ~Monster::new();
 
   let monsters = [monkey as ~Monster, gargoyle as ~Monster, chimera as ~Monster];
   monsters_attack(monsters);
