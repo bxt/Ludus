0920035ce1b5231dfc3cfb72418f6c559bb2120d
Bernhard Häussner <bxt@die-optimisten.net>
2013-11-04 13:48:52 +0100

    Replace set\(\[([^]]+)\]\) with {\1} in streets.py

--- unilectures.py/src/bxt/unilectures/crypto/hausaufg03/streets.py
+++ unilectures.py/src/bxt/unilectures/crypto/hausaufg03/streets.py
@@ -5,11 +5,11 @@ M = [(a,b,c,d,e) for a in n for b in n for c in n for d in n for e in n]
 
 if len(M) != 6**5: print("Fehler 01")
 
-K = {m for m in M if set([1,2,3,4]) <= set(m) or set([2,3,4,5]) <= set(m) or set([3,4,5,6]) <= set(m)}
+K = {m for m in M if {1,2,3,4} <= set(m) or {2,3,4,5} <= set(m) or {3,4,5,6} <= set(m)}
 print("|K| = " + str(len(K)))
 print("P(K) = " + str(Fraction(len(K), len(M))) + " = " + str(float(Fraction(len(K), len(M)))))
 
-G = {m for m in M if set([1,2,3,4,5]) <= set(m) or set([2,3,4,5,6]) <= set(m)}
+G = {m for m in M if {1,2,3,4,5} <= set(m) or {2,3,4,5,6} <= set(m)}
 print("|G| = " + str(len(G)))
 print("P(G) = " + str(Fraction(len(G), len(M))) + " = " + str(float(Fraction(len(G), len(M)))))
 
