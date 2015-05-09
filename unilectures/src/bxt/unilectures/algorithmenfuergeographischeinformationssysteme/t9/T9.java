2c942c44b2da3c96d7d344b1f72d7c854146b067
Bernhard Häussner <bxt@die-optimisten.net>
2015-05-09 19:17:42 +0200

    Add a shabby implementation of t9

--- unilectures/src/bxt/unilectures/algorithmenfuergeographischeinformationssysteme/t9/T9.java
+++ unilectures/src/bxt/unilectures/algorithmenfuergeographischeinformationssysteme/t9/T9.java
@@ -1,10 +1,14 @@
 package bxt.unilectures.algorithmenfuergeographischeinformationssysteme.t9;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class T9 {
@@ -25,9 +29,13 @@ public class T9 {
 	
 	public static void main(String[] args) {
 		T9 t9 = new T9("sample.txt");
-		int[] keys = t9.keypresses("bernhard");
-		System.out.println(Arrays.toString(keys));
-		System.out.println(t9.word(keys));
+		Stream.of("hello", "world", "the", "in", "and", "reuter", "bernhard", "people", "reliability", "many", "kind", "words", "for", "you").forEach(word -> {
+			int[] keys = t9.keypresses(word);
+			System.out.print(word);
+			System.out.print(Arrays.toString(keys));
+			System.out.print(t9.word(keys));
+			System.out.println();
+		});
 	}
 
 	public T9(String sampleTextFilename) {
@@ -39,10 +47,7 @@ public class T9 {
 				String curr = sc.next();
 				if(prev != null) {
 					String digram = prev + curr;
-					if(!digaramCounts.containsKey(digram))
-						digaramCounts.put(digram, 1);
-					else
-						digaramCounts.put(digram, digaramCounts.get(digram) + 1);
+					digaramCounts.put(digram, digaramCounts.getOrDefault(digram, 0) + 1);
 				}
 				sc.skip("[0-9]*");
 				prev = curr;
@@ -56,7 +61,66 @@ public class T9 {
 	}
 	
 	public String word(int[] keypresses) {
-		return "hello"; // TODO
+		
+		Node dummy = new Node(" ", null, 0);
+		List<Node> prev = new ArrayList<T9.Node>();
+		prev.add(dummy);
+		
+		for(int i = 0; i < keypresses.length; i++) {
+			List<Node> curr = new ArrayList<T9.Node>();
+			for (String letter : candidatesFor(keypresses[i])) {
+				Node pre = null;
+				int max = 0;
+				for(Node n : prev) {
+					int foo = n.probability + digaramCounts.getOrDefault(n.letter + letter, 0);
+					if (foo > max) {
+						max = foo;
+						pre = n;
+					}
+				}
+				Node m = new Node(letter, pre, max);
+				curr.add(m);
+			}
+			prev = curr;
+		}
+		
+		Node m;
+		{
+			Node pre = null;
+			int max = 0;
+			for(Node n : prev) {
+				int foo = n.probability + digaramCounts.getOrDefault(n.letter + " ", 0);
+				if (foo > max) {
+					max = foo;
+					pre = n;
+				}
+			}
+			m = new Node("", pre, max);
+		}
+		
+		String result = "";
+		while (m.predecessor != null) {
+			result = m.letter + result;
+			m = m.predecessor;
+		}
+		
+		return result;
+	}
+	
+	private List<String> candidatesFor(int key) {
+		return characterKey.entrySet().stream().filter(e -> e.getValue() == key).map(Entry::getKey).collect(Collectors.toList());
+	}
+	
+	private static class Node {
+		private String letter;
+		private Node predecessor;
+		private int probability;
+		
+		public Node(String letter, Node predecessor, int probability) {
+			this.letter = letter;
+			this.predecessor = predecessor;
+			this.probability = probability;
+		}
 	}
 	
 }
