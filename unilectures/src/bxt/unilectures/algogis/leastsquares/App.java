3a167b5484531dbba3696136cef1af0c1775cf67
Bernhard Häussner <bxt@die-optimisten.net>
2015-05-16 22:58:26 +0200

    Add input processing code for least squares assignment

---
+++ unilectures/src/bxt/unilectures/algogis/leastsquares/App.java
@@ -0,0 +1,46 @@
+package bxt.unilectures.algogis.leastsquares;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class App {
+	
+	
+	public static void main(String[] args) {
+		String input = ""
+				+ "4\n"
+				+ "5\n" // + "6\n" // probably wrong in assignment
+				+ "1 2 4.1 1\n"
+				+ "2 3 -7 1\n"
+				+ "3 4 1.1 1\n"
+				+ "4 1 1.2 1\n"
+				+ "4 2 5.4 1\n"
+				+ "\n"
+				+ "\n"
+				+ "\n"
+				;
+		Maseurements m = scanMaseurements(new Scanner(input));
+		
+		System.out.println(m);
+	}
+
+	private static Maseurements scanMaseurements(Scanner scanner) {
+		scanner.useLocale(Locale.US);
+		int pointsSize = scanner.nextInt();
+		int maseurementSize = scanner.nextInt();
+		
+		Maseurements maseurements =  new Maseurements(pointsSize);
+		
+		IntStream.range(0, maseurementSize).forEach(mn -> {
+			int from = scanner.nextInt();
+			int to = scanner.nextInt();
+			double value = scanner.nextDouble();
+			double variance = scanner.nextDouble();
+			maseurements.addMeasurement(from, to, value, variance);
+		});
+		
+		return maseurements;
+	}
+	
+}
---
+++ unilectures/src/bxt/unilectures/algogis/leastsquares/Maseurements.java
@@ -0,0 +1,120 @@
+package bxt.unilectures.algogis.leastsquares;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Maseurements {
+	
+	private Collection<Measurement> measurements = new LinkedList<Maseurements.Measurement>();
+	private int pointsSize;
+	
+	public Maseurements(int pointsSize) {
+		super();
+		this.pointsSize = pointsSize;
+	}
+	
+	public boolean addMeasurement(int from, int to, double value, double variance) {
+		return add(new Measurement(from, to, value, variance));
+	}
+	
+	public boolean add(Measurement measurement) {
+		checkBounds(measurement.getFrom());
+		checkBounds(measurement.getTo());
+		return measurements.add(measurement);
+	}
+
+	public int getPointsSize() {
+		return pointsSize;
+	}
+	
+	public int getMeasurementsSize() {
+		return measurements.size();
+	}
+
+	private void checkBounds(int point) {
+		if(point < 0) throw new IllegalArgumentException("Point numbers must be postive.");
+		if(point > pointsSize) throw new IllegalArgumentException("Point number exceeds point count.");
+	}
+	
+	public Collection<Measurement> getMeasurements() {
+		return measurements;
+	}
+
+	@Override
+	public String toString() {
+		return "Maseurements [measurements=" + measurements + ", pointsSize="
+				+ pointsSize + "]";
+	}
+
+	public static class Measurement {
+		private int from;
+		private int to;
+		private double value;
+		private double variance;
+		
+		public Measurement(int from, int to, double value, double variance) {
+			if(variance < 0 ) throw new IllegalArgumentException("Variance must be positive.");
+			this.from = from;
+			this.to = to;
+			this.value = value;
+			this.variance = variance;
+		}
+		
+		public int getFrom() {
+			return from;
+		}
+		public int getTo() {
+			return to;
+		}
+		public double getValue() {
+			return value;
+		}
+		public double getVariance() {
+			return variance;
+		}
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + from;
+			result = prime * result + to;
+			long temp;
+			temp = Double.doubleToLongBits(value);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			temp = Double.doubleToLongBits(variance);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			return result;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Measurement other = (Measurement) obj;
+			if (from != other.from)
+				return false;
+			if (to != other.to)
+				return false;
+			if (Double.doubleToLongBits(value) != Double
+					.doubleToLongBits(other.value))
+				return false;
+			if (Double.doubleToLongBits(variance) != Double
+					.doubleToLongBits(other.variance))
+				return false;
+			return true;
+		}
+		
+		@Override
+		public String toString() {
+			return "Measurement [from=" + from + ", to=" + to + ", value="
+					+ value + ", variance=" + variance + "]";
+		}
+		
+	}
+	
+}
