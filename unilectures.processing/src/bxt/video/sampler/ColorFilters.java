f7e20b36b069941226fa213534dfde4a35c93605
Bernhard Häussner <bxt@die-optimisten.net>
2015-11-02 21:52:56 +0100

    Refactor some bitwise operations

--- unilectures.processing/src/bxt/video/sampler/ColorFilters.java
+++ unilectures.processing/src/bxt/video/sampler/ColorFilters.java
@@ -7,12 +7,12 @@ public interface ColorFilters {
 	public static ColorFilter INVERT = (c) -> 0xffffff ^ c;
 	
 	public static float grey(int c) {
-		return ((c & 0xff) + ((c >>8) & 0xff) + ((c >>16) & 0xff)) / 768f;
+		return ((c & 0xff) + (c>>8 & 0xff) + (c>>16 & 0xff)) / 768f;
 	}
 
 	public static int grey(float percentage) {
 		int value = (int)(percentage * 255);
-		return (0xff<<24) + (value<<16) + (value<<8) + value;
+		return 0xff<<24 | value<<16 | value<<8 | value;
 	}
 
 }
--- unilectures.processing/src/bxt/video/sampler/ExtractHsColorFilter.java
+++ unilectures.processing/src/bxt/video/sampler/ExtractHsColorFilter.java
@@ -6,7 +6,7 @@ public class ExtractHsColorFilter implements ColorFilter {
 	
 	@Override
 	public int map(int color) {
-		float[] hsv = Color.RGBtoHSB((color>>16) & 0xff, (color>>8) & 0xff, color & 0xff, null);
+		float[] hsv = Color.RGBtoHSB(color>>16 & 0xff, color>>8 & 0xff, color & 0xff, null);
 		return Color.HSBtoRGB(hsv[0], hsv[1], 1);
 	}
 
