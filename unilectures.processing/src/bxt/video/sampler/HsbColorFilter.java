054f2cfd3a1b3bb7f28885e049f911d3b0199750
Bernhard Häussner <bxt@die-optimisten.net>
2015-11-03 13:01:46 +0100

    Add HsbColorFilter

---
+++ unilectures.processing/src/bxt/video/sampler/HsbColorFilter.java
@@ -0,0 +1,16 @@
+package bxt.video.sampler;
+
+import java.awt.Color;
+
+public abstract class HsbColorFilter implements ColorFilter {
+	
+	@Override
+	public int map(int color) {
+		float[] hsv = Color.RGBtoHSB(color>>16 & 0xff, color>>8 & 0xff, color & 0xff, null);
+		float[] result = map(hsv);
+		return Color.HSBtoRGB(result[0], result[1], result[2]);
+	}
+	
+	protected abstract float[] map(float[] color);
+
+}
